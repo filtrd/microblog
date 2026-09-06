16c64791ea6955a40efa591699f568994f17fda1
filtrd <167866186+filtrd@users.noreply.github.com>
2026-09-06 01:34:23 +0100

    Redirect logged-out users from home to index

--- home.php
+++ home.php
@@ -4,6 +4,11 @@ require_once __DIR__ . '/inc/database.php';
 require_once __DIR__ . '/inc/config.php';
 
 $user = current_user();
+if (!$user) {
+    header('Location: index.php');
+    exit;
+}
+
 $postError = get_flash('post_error');
 $postDraft = get_flash('post_draft') ?? '';
 $feedPageSize = (int)$feedPageSize;
